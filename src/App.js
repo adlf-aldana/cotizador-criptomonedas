a8d68c66f5d29f2b3e5d9403dc07542452de8577
Adolfo <yml.aldana@gmail.com>
2020-07-24 11:02:04 -0400

    styled contenedor e imagen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png'
+
+const Contenedor = styled.div`
+max-width: 900px;
+margin: 0 auto;
+@media (min-width: 992px){
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 2rem;
+}
+`;
+
+const Imagen = styled.img`
+max-width: 100%;
+margin-top: 5rem;
+`;
 
 function App() {
   return (
-    <h1>Cotizador de criptomonedas</h1>
+    <Contenedor>
+      <div>
+        <Imagen src={imagen} alt="Imagen crypto" />
+      </div>
+      <div></div>
+    </Contenedor>
   );
 }
 
