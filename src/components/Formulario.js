30c9a6ba789159cf58f5bc80a23f08daad9786af
Adolfo <yml.aldana@gmail.com>
2020-07-26 10:22:05 -0400

    uso de nuestro custom hook de forma inteligente

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -21,12 +21,19 @@ transition: background-color .3& ease;
 `;
 const Formulario = () => {
 
+    const MONEDAS = [
+        { codigo: 'USD', nombre: 'Dolar de Estados Unidos' },
+        { codigo: 'MXN', nombre: 'Peso Mexicano' },
+        { codigo: 'EUR', nombre: 'Euro' },
+        { codigo: 'GBP', nombre: 'Libra Esterlina' },
+    ]
+
     // Extraer datos
     // Utilizar hook useMoneda
     // debe estar en el orden del return, 
     // no importa el nombre, puede ser el mismo o diferente
-    const [moneda, SelectMoneda, actualizarState] = useMoneda();
-    
+    const [moneda, SelectMoneda, actualizarState] = useMoneda('Elige tu moneda: ', '', MONEDAS);
+
     return (
         <form>
 
--- src/components/Hooks/useMoneda.js
+++ src/components/Hooks/useMoneda.js
@@ -1,18 +1,21 @@
 import React, { Fragment, useState } from 'react';
 
-const useMoneda = () => {
+const useMoneda = (label, stateInicial, opciones) => {
 
-    const [state, actualizarState] = useState('')
+    const [state, actualizarState] = useState(stateInicial)
 
     const Seleccionar = () => (
         <Fragment>
-            <label>Moneda:</label>
+            <label>{label}</label>
             <select>
                 <option value="MXN">Peso Mexicano</option>
+                {opciones.map(opcion => (
+                    <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
+                ))}
             </select>
         </Fragment>
     );
-    
+
     // Retornar state, interfaz y funcion que modifica el state
     return [state, Seleccionar, actualizarState];
 }
