1c031a0d65500c824194cea8f285f85dda162de3
Adolfo <yml.aldana@gmail.com>
2020-07-26 14:19:46 -0400

    hoock useCriptomoneda, pasando criptomonedas al custom hook useMoneda desde una API

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import useMoneda from './Hooks/useMoneda';
+import useCriptomoneda from './Hooks/useCriptomoneda';
+import axios from 'axios';
 
 const Boton = styled.input`
 margin-top: 20px;
@@ -28,16 +30,30 @@ const Formulario = () => {
         { codigo: 'GBP', nombre: 'Libra Esterlina' },
     ]
 
+    const [listacripto, guardarCriptomonedas] = useState([])
     // Extraer datos
     // Utilizar hook useMoneda
     // debe estar en el orden del return, 
     // no importa el nombre, puede ser el mismo o diferente
     const [moneda, SelectMoneda] = useMoneda('Elige tu moneda: ', '', MONEDAS);
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu criptomoneda', '', listacripto)
+
+    // Ejecutar llamado a la API
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            const resultado = await axios.get(url);
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI()
+    }, []);
 
     return (
         <form>
 
             <SelectMoneda />
+
+            <SelectCripto />
 
             <Boton
                 type="submit"
